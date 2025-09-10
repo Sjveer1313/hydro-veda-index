f0330ef0e818a0bd94cbe8e94d234d4a862db8c8
Sukhmanpreet Singh <ssingh33_be23@thapar.edu>
2025-09-10 14:59:32 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
             <img 
               src="/lovable-uploads/HydroVeda1.png" 
               alt="HydroVeda" 
-              className="h-16 w-auto"
+              className="h-13 w-auto"
             />
           </Link>
 
